1bfd61714aa405c7999ffa5ea7d6a6d478d27df9
Jörn Brodthagen <brodthagen@melag.de>
2025-06-30 11:12:10 +0200

    Revert "remove solution"
    This reverts commit aa5c88c61efcb8361532f4848b8e03a4f3f64f15.

--- ProjectManagerSimulatorApi/Program.cs
+++ ProjectManagerSimulatorApi/Program.cs
@@ -1,10 +1,13 @@
 using ProjectManagerSimulatorApi.Repositories;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
 builder.Services.AddSingleton<EstimateRepository>();
 builder.Services.AddSingleton<DeadlineRepository>();
 
@@ -13,6 +16,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "v1");
+        app.MapScalarApiReference();
+    });
 }
 
 app.UseHttpsRedirection();
